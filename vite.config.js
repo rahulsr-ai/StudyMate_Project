09446ba5a85f7991c840f7831a78db833ab67b7c
eternal_student <student1742003@gmail.com>
2025-05-07 15:30:37 +0530

    backend domain add

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080", // Your backend URL
+        target: "https://studymaten.onrender.com/", // Your backend URL
         changeOrigin: true,
         secure: false, // Set to true if using HTTPS
       },
